7834b902973062f9b381ef5061bf516383932001
Aaron Dewes <aaron.dewes@protonmail.com>
2021-10-23 16:44:15 +0200

    Update index.ts

--- pages/api/documents/index.ts
+++ pages/api/documents/index.ts
@@ -11,5 +11,5 @@ export const config = {
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // Trick Umbrel into displaying Citadel ads
-  return res.json({ ok: true, key: "This server has been deleted. Using Umbrel? We recommend switching to Citadel, Umbrel treats developers (https://twitter.com/AaronDewes/status/1449445615725854720) and users badly. twitter.com/runcitadel" });
+  return res.json({ ok: true, key: "Hi! This isn't a link to share, the link generation has stopped and this server has been deleted. Regaring your node, did you know the software you're running is a commercial project which doesn't follow the open source guidelines and thus isn't open source? I (the person who runs this server Umbrel is using) recommend switching to Citadel, Umbrel treats developers (https://twitter.com/AaronDewes/status/1449445615725854720) and users badly. twitter.com/runcitadel. Also please have a look at https://twitter.com/runcitadel/status/1451798015312744459?s=20 and the replies to see more reasons for this message. Umbrel will stop using this server in the next update, but on this version, you can still see it. Citadel has dark mode, more features and isn't commercial. https://runcitadel.space will show more on how to switch and how to setup a new node soon." });
 };
